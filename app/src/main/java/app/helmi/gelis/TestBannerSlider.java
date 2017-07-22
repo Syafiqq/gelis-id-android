c390ec761c18d9c3fc0f3000819559871dc565f9
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-22 12:03:27 +0700

    Completing banner slider

--- app/src/main/java/app/helmi/gelis/TestBannerSlider.java
+++ app/src/main/java/app/helmi/gelis/TestBannerSlider.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import ss.com.bannerslider.banners.RemoteBanner;
+import ss.com.bannerslider.views.BannerSlider;
 
 public class TestBannerSlider extends AppCompatActivity
 {
@@ -28,6 +30,15 @@ public class TestBannerSlider extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+
+        BannerSlider bannerSlider = (BannerSlider) findViewById(R.id.banner_slider1);
+        //add banner using image url
+        bannerSlider.addBanner(new RemoteBanner("http://gelis.id/admin/geventbanner/1500659005-2017-07-21.jpg"));
+        bannerSlider.addBanner(new RemoteBanner("http://gelis.id/admin/geventbanner/1500659005-2017-07-21.jpg"));
+        bannerSlider.addBanner(new RemoteBanner("http://gelis.id/admin/geventbanner/1500659005-2017-07-21.jpg"));
+        bannerSlider.addBanner(new RemoteBanner("http://gelis.id/admin/geventbanner/1500659005-2017-07-21.jpg"));
+        bannerSlider.addBanner(new RemoteBanner("http://gelis.id/admin/geventbanner/1500659005-2017-07-21.jpg"));
+        bannerSlider.addBanner(new RemoteBanner("http://gelis.id/admin/geventbanner/1500659005-2017-07-21.jpg"));
     }
 
 }
