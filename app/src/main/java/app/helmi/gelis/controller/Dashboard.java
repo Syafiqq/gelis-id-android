fde46a071585bdaeb574a2a67b139c2b7ec81893
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-22 23:38:34 +0700

    Add Dashboard activity

---
+++ app/src/main/java/app/helmi/gelis/controller/Dashboard.java
@@ -0,0 +1,17 @@
+package app.helmi.gelis.controller;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import app.helmi.gelis.R;
+
+public class Dashboard extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        
+    }
+}
