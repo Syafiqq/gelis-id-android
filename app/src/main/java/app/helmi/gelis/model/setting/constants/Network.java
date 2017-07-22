bd9c3f7eb70cce349dd6320638117b06c315b407
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-23 05:10:24 +0700

    Add Network Constants

---
+++ app/src/main/java/app/helmi/gelis/model/setting/constants/Network.java
@@ -0,0 +1,18 @@
+package app.helmi.gelis.model.setting.constants;
+
+/*
+ * This <Gelis> created by : 
+ * Name         : syafiq
+ * Date / Time  : 22 July 2017, 11:46 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+public class Network
+{
+    public static final String API_BASE_URL = "https://gelis.id/";
+    public static final String BANNER_API = "admin/index.php/eventbanner/";
+    public static final String BANNER_ASSET_API = "admin/geventbanner/";
+    public static final String EVENT_API = "admin/index.php/eventdata/";
+    public static final String EVENT_ASSET_API = "admin/gambarevent/";
+}
